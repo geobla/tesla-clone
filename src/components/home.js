a8aefad2ba8838c43791f82cdabf71e189143b0b
geobla <geomav07@gmail.com>
2022-08-31 08:14:21 +0300

    newCommit

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function home() {
+  return <Container>home</Container>;
+}
+
+export default home;
+const Container = styled.div`
+  height: 100vh;
+`;
